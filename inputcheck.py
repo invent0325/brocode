919181f978fcf1c7ad5a01509ab922839817c717
Lawrence <lpinto1204@gmail.com>
2021-04-02 17:46:08 +0530

    changes

---
+++ inputcheck.py
@@ -0,0 +1,8 @@
+import string
+
+s = "The quick brown fox jumped over the lazy dog!"
+print( string.capwords( s ) )
+print( s.upper() )
+print( s.lower() )
+print( s.split() )
+print( s.find( "dog" ) )
